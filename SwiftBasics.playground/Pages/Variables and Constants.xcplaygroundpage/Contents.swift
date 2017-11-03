ddf65e56b0fa6c9166a3d04025273123c7a86ee3
Karthik Palusa <karthik.palusa@gmail.com>
2017-11-02 19:59:22 -0600

    Basic Guided Swift Walkthrough, and First Commit of FunFacts Demo
    First Commit; open playground content and demo application. Each file
    is labeled within playground itself. FunFacts commit has issues.

---
+++ SwiftBasics.playground/Pages/Variables and Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+// Variables
+
+var str = "Hello, playground"
+str = "Hello, world!"
+
+var number = 10
+number = 20
+
+// Constants
+
+let language = "Swift"
+
+// Naming Conventions
+
+/*
+ Rule #1: Spaces not allowed
+ let programming language = "Objective-C"
+ */
+
+/*
+ Rule #2: Use camelCase
+ let programming language = Objective-C // Wrong!
+ */
+
+let programmingLanguage = "Objective-C"
+let favoriteProgrammingLanguage = "Swift"
+
+var 😺 = "cat"
+😺
