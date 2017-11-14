73666f08d5f551caa3f4820a785769fe7e7e8916
Karthik Palusa <karthik.palusa@gmail.com>
2017-11-14 13:26:00 -0700

    Working Application
    FunFacts is now a working app.

---
+++ Swift-Training/FunFacts/FunFacts/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Karthik Palusa on 10/23/17.
+//  Copyright © 2017 Karthik Palusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    
+    let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factProvider.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFact() {
+        funFactLabel.text = factProvider.randomFact()
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+    }
+    
+}
+
