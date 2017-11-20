276ed40e16fa64d4360f5e3456bee20057593a12
Karthik Palusa <karthik.palusa@gmail.com>
2017-11-19 17:48:52 -0700

    Commit 3
    Took Out The FunFact App, to make navigation in folders easier.

---
+++ EnumerationsOptional.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,114 @@
+struct Person {
+    let firstName: String
+    let middleName: String?
+    let lastName: String
+    
+    func fullName() -> String {
+        if middleName == nil {
+            return firstName + " " + lastName
+        } else {
+            return firstName + " " + middleName! + " " + lastName
+        }
+    }
+    
+}
+
+let me = Person(firstName: "Karthik", middleName: nil, lastName: "Palusa")
+me.fullName()
+
+let airportCodes = ["CDG" : "Charles De Gaulle"]
+let newYorkAirport = airportCodes["JFK"]
+
+// Optional Binding Using If Let
+
+if let newYorkAirport = airportCodes["JFK"] {
+    print (newYorkAirport)
+} else {
+    print("Whoops! That key does not exist!")
+}
+
+let weatherDictionary: [String: [String: String]] = [
+    "currently"  : ["temperature" : "22.3"],
+    "daily" : ["temperature" : "22.3"],
+    "weekly" : ["temperature" : "22.3"]
+]
+
+if let dailyWeather = weatherDictionary["daily"] {
+    if let highTemp = dailyWeather["temperature"] {
+        print(highTemp)
+    }
+}
+
+if let dailyWeather = weatherDictionary["daily"], let highTemperature = dailyWeather["temperature"] {
+    print(highTemperature)
+}
+
+// Downsides to Using If LEt
+struct Friend {
+    let name: String
+    let age: String
+    let address: String?
+}
+
+func new(friendDictionary: [String: String]) -> Friend? {
+    if let name = friendDictionary["name"], let age = friendDictionary["age"] {
+        let address = friendDictionary["address"]
+        return Friend(name: name, age: age, address: address)
+    } else {
+        return nil
+    }
+}
+
+// Early Exit
+
+func newFriend (friendDictionary: [String: String]) -> Friend? {
+    guard let name = friendDictionary["name"], let age = friendDictionary["age"] else {
+        return nil
+    }
+    
+    let address = friendDictionary["address"]
+    
+    return Friend(name: name, age: age, address: address)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
