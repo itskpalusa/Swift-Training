7e4c817da0554548fabc0e4a9e2954a5c1d83c54
Karthik Palusa <karthik.palusa@gmail.com>
2017-11-21 21:42:35 -0700

    Protocol Basics
    Includes some basic protocol code with examples

---
+++ Protocols.playground/Contents.swift
@@ -0,0 +1,215 @@
+protocol FullNameable {
+    var fullName: String { get }
+}
+
+struct User: FullNameable {
+    var fullName: String
+}
+
+let user = User(fullName: "John Smith")
+
+struct Friend: FullNameable {
+    let firstName: String
+    let middleName: String
+    let lastName: String
+    
+    var fullName: String {
+        return "\(firstName) \(middleName) \(lastName)"
+    }
+}
+
+let friend = Friend(firstName: "Taylor", middleName: "Alison", lastName: "Swift")
+friend.fullName
+
+import Foundation
+
+enum EmployeeType {
+    case manager
+    case traditional
+}
+
+protocol Payable {
+    func pay() -> Paycheck
+}
+
+struct Paycheck {
+    let base: Double
+    let benefits: Double
+    let deductions: Double
+    let vacation: Double
+}
+
+class Employee {
+    let name: String
+    let address: String
+    let startDate: Date
+    let type: EmployeeType
+    
+    init(name: String, address: String, startDate: Date, type: EmployeeType) {
+        self.name = name
+        self.address = address
+        self.startDate = startDate
+        self.type = type
+    }
+}
+
+class HourlyEmployee: Employee, Payable {
+    var hourlyWage = 15.0
+    var hoursWorked = 0.0
+    var vacation = 0
+    
+    func pay() -> Paycheck {
+        let base = hoursWorked * hourlyWage
+        return Paycheck(base: base, benefits: 0, deductions: 0, vacation: 0)
+    }
+}
+
+class SalariedEmployee: Employee, Payable{
+    var salary = 50000.00
+    var benefits = 1000.00
+    var deductions = 0.0
+    var vacation = 2.0
+    
+    func pay() -> Paycheck {
+        let monthly = salary/12
+        return Paycheck(base: monthly, benefits: benefits, deductions: deductions, vacation: vacation)
+    }
+}
+
+func pay(employee: Payable) {
+    employee.pay()
+}
+
+let employee = HourlyEmployee(name: "Karthik", address: "someAddress", startDate: Date(), type: .traditional)
+pay(employee: employee)
+
+// Loosely Related Types
+
+protocol Blendable {
+    func blend()
+}
+
+class Fruit: Blendable {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func blend() {
+        print("I'm mush!")
+    }
+}
+
+class Dairy {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Cheese: Dairy {}
+
+class Milk: Dairy, Blendable {
+    func blend() {
+        print("I haven't changed at all! I'm still milk fam!")
+    }
+}
+
+func makeSmoothie(with ingredients: [Blendable]) {
+    for ingredient in ingredients {
+        ingredient.blend()
+    }
+}
+
+let strawberry = Fruit(name: "Strawberry")
+let cheese = Cheese(name: "Chedder")
+let chocolateMilk = Milk(name: "Chocolate")
+
+let ingredients: [Blendable] = [strawberry, chocolateMilk]
+
+makeSmoothie(with: ingredients)
+
+// Random Number Generator
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+d6.roll()
+
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+// Prints "Here's a random number: 0.37464991998171"
+print("And another one: \(generator.random())")
+// Prints "And another one: 0.729023776863283"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
